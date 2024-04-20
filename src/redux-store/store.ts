cb431b8d442be4375425964e0eda700f6c1b2134
sricharankrishnan <sricharan.krishnan@gmail.com>
2024-04-20 14:46:58 +0530

    [feat] Redux Dev Tools
    - instaled the old redux-devtools-extension
    - done through --legacy-peer-deps
    - installed the redux extension on chrome browser and checked state
    - updated app screenshot

--- src/redux-store/store.ts
+++ src/redux-store/store.ts
@@ -1,11 +1,12 @@
 /* node modules */
 import { createStore } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 /* app imports */
 import rootReducer from "./reducer";
 
 /* store */
-const reduxStore = createStore(rootReducer);
+const reduxStore = createStore(rootReducer, composeWithDevTools());
 
 /* exports */
 export default reduxStore;
